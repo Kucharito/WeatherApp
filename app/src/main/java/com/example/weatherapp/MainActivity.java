2e0cfb0d4cf860991b9db7080c3c2e9cc31db05d
Adam Kuchar <adamkuchar10@gmail.com>
2025-07-10 17:50:02 +0200

    fixed keyboard

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.weatherapp;
 import android.annotation.SuppressLint;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -62,5 +68,25 @@ public class MainActivity extends AppCompatActivity implements OnForecastItemCli
         if (weatherFragment != null) {
             weatherFragment.updateWeatherFromForecast(item);
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if(ev.getAction() == MotionEvent.ACTION_DOWN){
+            View v = getCurrentFocus();
+            if (v instanceof EditText){
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+                    // Click outside the EditText, hide the keyboard
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    }
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
